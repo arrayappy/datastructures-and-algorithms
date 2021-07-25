e8cac7132fc11ea267d2109c11aa7ee7cd0b14b1
arrayappy <arrayappy@gmail.com>
2021-07-25 13:10:54 +0530

    Implementation of Binary Search

--- Searching/BinarySearch.cpp
+++ Searching/BinarySearch.cpp
@@ -3,22 +3,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr[], int left, int right, int target)
+int binarySearch(int arr[], int low, int high, int target)
 {
-    while(left<=right)
+    while(low<=high)
     {   
-        int mid=(left+right)/2;
+        int mid=(low+high)/2;
         if(arr[mid]==target)
         {
             return mid;
         }
         if(arr[mid]<target)
         {
-            return binarySearch(arr, mid+1, right, target);
+            return binarySearch(arr, mid+1, high, target);
         }
         if(arr[mid]>target)
         {
-            return binarySearch(arr, left, mid-1, target);
+            return binarySearch(arr, low, mid-1, target);
         }
     }
     return -1; 
